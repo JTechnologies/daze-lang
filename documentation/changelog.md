5f23a1fa67ed2ceaa1dcba97b30566f8d4638b2c
Judah B <me@jtechnologies.xyz>
2021-11-19 09:23:13 -0600

    Move v2 engine to be main engine. Update changelog & docs.

--- documentation/changelog.md
+++ documentation/changelog.md
@@ -1,5 +1,21 @@
 # Changelog
-## v0.2.0a
+## v2.0.0a
+### Changes
+- [x] Seperate the `toHtml()` function into `toHtml()`, `parseSite()`, and `parseTag()`
+- [X] Remove `$content` & `$variables` headers.
+- [X] Add brand-new document types. `$script` for scripts & `$site` for websites and other digital content.
+- [X] Improve Escaping
+### Known Errors
+- div elements still do not work. This will be changed shortly with the new engine.
+### Fixed Errors
+- Improve escaping system
+### Language Changes
+- Merge Variables and Tags into one section (removes `$variables` & `$content`)
+- Add new `$script` & `$site` headers to determine doctype
+### Notes
+- Engine v2 is still a work-in-progress. The `daze` tool will soon have a command to test the beta.
+
+## v1.2.0a
 ### Changes
 - [x] Add new option to output partial html to the engine
 - [x] Allow setting variables to variables
@@ -9,7 +25,7 @@
 - [x] Add support for JavaScript
 - [x] Add support for CSS
 ### Known Errors
-- div elements do not work. This should be fixed by v0.4.0
+- div elements do not work. This should be fixed by v1.4.0
 ### Fixed Errors
 *none*
 ### Language Changes
--- src/engine.py
+++ src/engine.py
@@ -1,117 +1,185 @@
-def split(delimiters, string, maxsplit=0):
-    import re
-    regexPattern = '|'.join(map(re.escape, delimiters))
-    return re.split(regexPattern, string, maxsplit)
-
-def toHtml(input, outputPart="full"):
-  head=input.split("$content")[0]
-  body=input.split("$content")[1]
-  head=split(["+!","+%","##"], head.strip("\n").replace("+%","+%~mv").replace("+!", "+!~v").replace("##","##~c"))
-  for i in range(len(head)):
-      head[i]=head[i].strip("\n")
-  head.pop(0)
-  variables={}
-  metaVariables={}
-  for i in range(len(head)):
-    if head[i][0]=="~" and head[i][1]=="v":
-      variables[head[i].split("=")[0][2::]]=head[i].split("=")[1]
-    elif head[i][0]=="~" and head[i][1]=="m" and head[i][2]=="v":
-      metaVariables[head[i].split("=")[0][3::]]=head[i].split("=")[1]
-    elif head[i][0]=="~" and head[i][1]=="c":
-      print(f"Comment at line {i+2}: '{head[i][2::]}'")
+escapeProtect = "🵋"
+def parseTag(line, skip=0):
+    skipNext = skip
+    split = line.replace("\:", "🵋")
+    split = line.split(":")
+    for i in range(len(split)-1):
+        split[i] = split[i].replace("🵋", ":")
+        split[i] = split[i].replace("\(", "🵋")
+    split[0] = split[0].replace("(", "")
+    for i in range(len(split)-1):
+        split[i].replace("🵋", "(")
+    if len(split) != 2:
+        for i in range(1,len(split)):
+            if skipNext!=0:
+                skipNext = skipNext-1
+                pass
+            elif skipNext==0:
+                split[i] = split[i].replace("\)", "🵋")
+                split[i] = split[i].replace(")", "")
+                split[i] = split[i].replace("🵋", ")")
+                if split[i][0] == " ":
+                    if split[i][1] == "'" or split[i][1] == '"':
+                        split[i] = split[i][2:-1]
+                    elif split[i][1] == "(":
+                        print(split[i])
+                        this = split[i][1:]
+                        next = split[i+1]
+                        fl = True
+                        c=1
+                        p=next
+                        while fl==True:
+                            if p[0] == "(" or p[1] == "(":
+                                next=f'{next}:{split[i+c]}'
+                                p = split[i+c]
+                                skipNext=skipNext+1
+                                c=c+1
+                            else:
+                                skipNext=skipNext+1
+                                fl = False
+                        while next[-1] == ")":
+                            if next[-1] == ")":
+                                next = next[0:-1]
+                        addBack = 0
+                        for j in range(len(next)):
+                            if(next[j] == "("):
+                                addBack = addBack + 1
+                        while addBack>0:
+                            next=next+")"
+                            addBack = addBack-1
+                        print(i)
+                        split[i] = parseTag(f"{this}:{next}", skip=skipNext)
+                elif split[i][0] == "'" or '"':
+                    split[i] = split[1][2:-1]
+                elif split[i][0] == "(":
+                        print(split[i])
+                        this = split[i][1:]
+                        next = split[i+1]
+                        fl = True
+                        c=1
+                        p=next
+                        while fl==True:
+                            if p[0] == "(" or p[1] == "(":
+                                next=f'{next}:{split[i+c]}'
+                                p = split[i+c]
+                                skipNext=skipNext+1
+                                c=c+1
+                            else:
+                                skipNext=skipNext+1
+                                fl = False
+                        while next[-1] == ")":
+                            if next[-1] == ")":
+                                next = next[0:-1]
+                        addBack = 0
+                        for j in range(len(next)):
+                            if(next[j] == "("):
+                                addBack = addBack + 1
+                        while addBack>0:
+                            next=next+")"
+                            addBack = addBack-1
+                        print(i)
+                        split[i] = parseTag(f"{this}:{next}", skip=skipNext)
     else:
-      print(f"!! Daze Syntax Error: Invalid Character in $variables at line {i+2}")
-  metahtml=""
-  for i in list(variables):
-    contents=variables[i]
-    for j in variables.keys():
-      if variables[j][0]=="'"or variables[j][0]=='"':
-        contents=contents.replace(f"!{j} ",variables[j][1:-1])
-      else:
-        contents=contents.replace(f"!{j} ",variables[j])
-    variables[i]=contents
-  for i in list(metaVariables):
-    contents=metaVariables[i]
-    for j in metaVariables.keys():
-      if metaVariables[j][0]=="'"or metaVariables[j][0]=='"':
-        contents=contents.replace(f"%{j} ",metaVariables[j][1:-1])
-      else:
-        contents=contents.replace(f"%{j} ",metaVariables[j])
-    metaVariables[i]=contents
-  for i in list(metaVariables):
-    contents=metaVariables[i]
-    for j in variables.keys():
-      if variables[j][0]=="'"or variables[j][0]=='"':
-        contents=contents.replace(f"!{j} ",variables[j][1:-1])
-      else:
-        contents=contents.replace(f"!{j} ",variables[j])
-    metaVariables[i]=contents
-  for i in range(len(metaVariables)):
-      if list(metaVariables)[i]=="title":
-          metahtml=metahtml+f'<title>{metaVariables[list(metaVariables)[i]][1:-1]}</title>\n'
-      else:
-          metahtml=metahtml+f'<meta name="{list(metaVariables)[i][1:-1]}" content="{metaVariables[i][1:-1]}">\n'
-  
-  body=body.strip("\n").split("(")
-  body.pop(0)
-  bodyhtml=""
-  for i in range(len(body)):
-      part=body[i].split(")")
-      element=part[0].strip(" ").split(": ")
-      contents=element[1]
-      if contents[0]=='"' and contents[-1]=='"' or contents[0]=="'" and contents[-1]=="'":
-          contents=contents[1:-1]
-      for i in variables.keys():
-        if variables[i][0]=="'"or variables[i][0]=='"':
-          contents=contents.replace(f"!{i} ",variables[i][1:-1])
+        split[1] = split[1].replace("\)", "🵋")
+        split[1] = split[1].replace(")", "")
+        split[1] = split[1].replace("🵋", ")")
+        if split[1][0] == " ":
+            if split[1][1] == "'" or '"':
+                split[1] = split[1][2:-1]
+            elif split[1][1] == "(":
+                print("ERR")
+        elif split[1][0] == "'" or '"':
+            split[1] = split[1][2:-1]
+        elif split[1][0] == "(":
+            pass
+    #print([split[0], split[1]])
+    return [split[0],split[1]]
+def parseSite(input):
+    lines = []
+    comments=[]
+    variables={}
+    metavariables={}
+    tags=[]
+    for line in input:
+        lines.append(line)
+    for line in lines:
+        if line[:2] == "##":
+            comments.append(line[2:])
+        elif line[:2] == "+!":
+            variable = line[2:].replace(" = ", "=").split("=")
+            if variable[1][0] == "'" or variable[1][0] == '"':
+                variable[1] =['str', variable[1][1:-1]]
+            elif variable[1] == "True" or variable[1] == "False":
+                variable[1]=['bol', variable[1]]
+            elif int(variable[1]):
+                variable[1]=['int', variable[1]]
+            elif variable[1][0] == "!":
+                variable[1]=variables[variable[1]]
+            elif variable[1][0] == "%":
+                variable[1]=metavariables[variable[1]]
+            else:
+                #TODO: GET CURRENT LINE NUMBER FOR ERROR
+                return("!!Error on line (tba): Invalid Variable Type")
+            variables[variable[0]]=variable[1]
+        elif line[:2] == "+%":
+            variable = line[2:].replace(" = ", "=").split("=")
+            if variable[1][0] == "'" or variable[1][0] == '"':
+                variable[1] =['str', variable[1][1:-1]]
+            elif variable[1] == "True" or variable[1] == "False":
+                variable[1]=['bol', variable[1]]
+            elif int(variable[1]):
+                variable[1]=['int', variable[1]]
+            elif variable[1][0] == "!":
+                variable[1]=variables[variable[1]]
+            elif variable[1][0] == "%":
+                variable[1]=metavariables[variable[1]]
+            else:
+                #TODO: GET CURRENT LINE NUMBER FOR ERROR
+                return("!!Error on line (tba): Invalid MetaVariable Type")
+            metavariables[variable[0]]=variable[1]
+        elif line[0] == "(":
+            tags.append(parseTag(line))
+            #print(parseTag(line))
+        elif line[0] == "$":
+            pass
+        elif line[0] == "+":
+            working = line
+            working = working[1:]
+            working = working.replace(" = ", "=").split("=")
+            if working[1][0] == "!":
+                working[1]=variables[working[1][1:]][1]
+            elif working[1][0] == "'" or working[1][0] == '"':
+                working[1]=working[1][1:-1] 
+            tag = tags[-1]
+            #print(tag)
+            if len(tag) == 2:
+                tag.append([working])
+            elif len(tag) == 3:
+                tag[2].append(working)
+            else:
+                return("!!Error: Internal Error. Submit an issue with code 'r01:ev2:086'")
+            #print(tag)
         else:
-          contents=contents.replace(f"!{i} ",variables[i])
-      attributes_unprocessed=part[1].strip("\n").split("+")
-      attributes={}
-      for i in range(len(attributes_unprocessed)):
-        attributes_unprocessed[i]=attributes_unprocessed[i].strip("\n")
-      attributes_unprocessed.pop(0)
-      for i in range(len(attributes_unprocessed)):
-        attributes[attributes_unprocessed[i].split("=")[0]]=attributes_unprocessed[i].split("=")[1]
-      strAttributes=""
-      for i in attributes.keys():
-          strAttributes=strAttributes+f' {i}="{attributes[i][1:-1]}"'
-      if element[0]=="img":
-        bodyhtml=bodyhtml+f'<img src="{contents}" {strAttributes}>\n'
-      elif element[0]=="linkScript":
-        bodyhtml=bodyhtml+f'<script src="{contents}" {strAttributes}></script>\n'
-      elif element[0]=="linkStyle":
-        bodyhtml=bodyhtml+f'<link rel="stylesheet" href="{contents}" {strAttributes}>\n'
-      elif element[0]=="link":
-        bodyhtml=bodyhtml+f'<link href="{contents}" {strAttributes}>\n'
-      elif element[0]=="script":
-        bodyhtml=bodyhtml+f'<script {strAttributes}>\n{contents}\n</script>\n'
-      elif element[0]=="style":
-        bodyhtml=bodyhtml+f'<style {strAttributes}>\n{contents}\n</style>\n'
-      else:
-        bodyhtml=bodyhtml+f'<{element[0]}{strAttributes}>{contents}</{element[0]}>\n'
-  if outputPart=="full":
-      return(f"""<!DOCTYPE html>
-<html>
+            return(f"!!Error on line (tba): Unknown line start. {line}")
+    return comments, variables, metavariables, tags
+def toHtml(input, part="head"):
+    textInput = input
+    input = input.replace("; ", "\n").replace(";", "\n")
+    input = input.replace("\\\n", ";")
+    input = input.splitlines()
+    tags = []
+    comments = []
+    variables = {}
+    metavariables = {}
+    print(input, "\n")
+    if input[0] == "$site":
+        comments, variables, metavariables, tags = parseSite(input)
+    elif input[0] == "$script":
+        return "!!Error: Attempt to compile pure script into HTML"
+    else:
+        return f"!!Error on line 1: Unknown Daze type '{input[0]}'"
+    output = input 
+    return metavariables, variables, tags, comments
 
-<!-- Site compiled from Daze -->
-
-<head>
-
-{metahtml}
-</head>
-<body>
-{bodyhtml}
-</body>
-</html>""")
-  elif outputPart=="head":
-      return(f"""<!-- Part compiled from Daze -->
-{metahtml}
-<!-- End of Part -->
-""")
-  elif outputPart=="body":
-      return(f"""<body>
-<!-- Part compiled from Daze -->
-{bodyhtml}
-<!-- End of Part -->
-""")
+# Testing Area
+print(toHtml("$site; +!lol = 'rofl'; (div: (div: (div: (p: 'hey')))); +lol='lol'"))
